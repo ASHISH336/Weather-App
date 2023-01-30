2d8063fecfb9188c617504a735048c3a01af9c0d
ASHISH336 <singhaks755@gmail.com>
2023-01-30 19:59:09 +0530

    Second Commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6109c9f2d6msh2931197e355ca7dp1326ccjsnd539eb8b8392",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "f7bacbcfdc394c27c4109749b33669a6";
